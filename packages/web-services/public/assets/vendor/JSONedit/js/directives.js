013987708dd9b10fa1720915156f9d44c2576d45
Andrew Hass <ahass@jellyvision.com>
2014-08-07 15:51:44 -0500

    Adding a JSON visualizer...

--- packages/web-services/public/assets/vendor/JSONedit/js/directives.js
+++ packages/web-services/public/assets/vendor/JSONedit/js/directives.js
@@ -1,27 +1,6 @@
 'use strict';
-
-angular.element(document).ready(function() {
-    //Fixing facebook bug with redirect
-    if (window.location.hash === '#_=_') window.location.hash = '#!';
-
-    //Then init the app
-    angular.bootstrap(document, ['mean']);
-
-});
-
-// Dynamically add angular modules declared by packages
-var packageModules = [];
-for (var index in window.modules) {
-    angular.module(window.modules[index].module, window.modules[index].angularDependencies || []);
-    packageModules.push(window.modules[index].module);
-}
-
-// Default modules
-var modules = ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.router', 'ui.tree', 'angular-carousel'];
-modules = modules.concat(packageModules);
-
-// Combined modules
-var app = angular.module('mean', modules);
+console.log("HI");
+var app = angular.module('JSONedit', ['ui']);
 
 // fix ui-multi-sortable to y-axis
 app.value('ui.config', {
