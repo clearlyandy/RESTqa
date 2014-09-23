2140822edfef2f77c866a6026d08d3afb4da7593
Andrew Hass <ahass@jellyvision.com>
2014-09-23 15:18:48 -0500

    Now extending/merging assertion objects properly.

--- packages/web-services/public/controllers/requests.js
+++ packages/web-services/public/controllers/requests.js
@@ -95,7 +95,7 @@ angular.module('mean.web-services').controller('RequestsController', ['$scope', 
 
 
                 $scope.expecteddata = $scope.actualdata;
-                $scope.expecteddata.body = $scope.extendDeep(angular.copy($scope.actualdata.body), xxx);
+                $scope.expecteddata.body = extend(angular.copy($scope.actualdata.body), xxx);
 
                 $scope.hasResponse = true;
             });
@@ -124,20 +124,97 @@ angular.module('mean.web-services').controller('RequestsController', ['$scope', 
             });
         };
 
-        $scope.extendDeep = function extendDeep(dst) {
-            angular.forEach(arguments, function(obj) {
-            if (obj !== dst) {
-              angular.forEach(obj, function(value, key) {
-                if (dst[key] && dst[key].constructor && dst[key].constructor === Object) {
-                  extendDeep(dst[key], value);
-                } else {
-                  dst[key] = value;
+        var extend = function() {
+            var options, name, src, copy, copyIsArray, clone, target = arguments[0] || {},
+            i = 1,
+            length = arguments.length,
+            deep = false,
+            toString = Object.prototype.toString,
+            hasOwn = Object.prototype.hasOwnProperty,
+            push = Array.prototype.push,
+            slice = Array.prototype.slice,
+            trim = String.prototype.trim,
+            indexOf = Array.prototype.indexOf,
+            class2type = {
+              "[object Boolean]": "boolean",
+              "[object Number]": "number",
+              "[object String]": "string",
+              "[object Function]": "function",
+              "[object Array]": "array",
+              "[object Date]": "date",
+              "[object RegExp]": "regexp",
+              "[object Object]": "object"
+            },
+            jQuery = {
+              isFunction: function (obj) {
+                return jQuery.type(obj) === "function"
+              },
+              isArray: Array.isArray ||
+              function (obj) {
+                return jQuery.type(obj) === "array"
+              },
+              isWindow: function (obj) {
+                return obj != null && obj == obj.window
+              },
+              isNumeric: function (obj) {
+                return !isNaN(parseFloat(obj)) && isFinite(obj)
+              },
+              type: function (obj) {
+                return obj == null ? String(obj) : class2type[toString.call(obj)] || "object"
+              },
+              isPlainObject: function (obj) {
+                if (!obj || jQuery.type(obj) !== "object" || obj.nodeType) {
+                  return false
                 }
-              });
+                try {
+                  if (obj.constructor && !hasOwn.call(obj, "constructor") && !hasOwn.call(obj.constructor.prototype, "isPrototypeOf")) {
+                    return false
+                  }
+                } catch (e) {
+                  return false
+                }
+                var key;
+                for (key in obj) {}
+                return key === undefined || hasOwn.call(obj, key)
+              }
+            };
+          if (typeof target === "boolean") {
+            deep = target;
+            target = arguments[1] || {};
+            i = 2;
+          }
+          if (typeof target !== "object" && !jQuery.isFunction(target)) {
+            target = {}
+          }
+          if (length === i) {
+            target = this;
+            --i;
+          }
+          for (i; i < length; i++) {
+            if ((options = arguments[i]) != null) {
+              for (name in options) {
+                src = target[name];
+                copy = options[name];
+                if (target === copy) {
+                  continue
+                }
+                if (deep && copy && (jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)))) {
+                  if (copyIsArray) {
+                    copyIsArray = false;
+                    clone = src && jQuery.isArray(src) ? src : []
+                  } else {
+                    clone = src && jQuery.isPlainObject(src) ? src : {};
+                  }
+                  // WARNING: RECURSION
+                  target[name] = extend(deep, clone, copy);
+                } else if (copy !== undefined) {
+                  target[name] = copy;
+                }
+              }
             }
-            });
-            return dst;
-        };
+          }
+          return target;
+        }
 
         $scope.updateAssertions = function() {
             $scope.assertions = {};
@@ -158,13 +235,10 @@ angular.module('mean.web-services').controller('RequestsController', ['$scope', 
                         var newArr = [];
                         newArr[s.index] = assertion;
                         assertion = newArr;
-
-                        console.log(s.index);
                         s = s.parent;
                     }
                 }
-                console.log(assertion);
-                $scope.assertions = $scope.extendDeep($scope.assertions, assertion);
+                $scope.assertions = extend(true, {}, $scope.assertions, assertion);
             }
 
             var request = $scope.request;
@@ -390,7 +464,7 @@ angular.module('mean.web-services').controller('RequestsController', ['$scope', 
             };
             scope.traceP = function(val) {
                 console.log(val);
-            }
+            };
 
             //////
             // Template Generation
