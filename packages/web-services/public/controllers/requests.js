04793eef8f3a03ba4b0ee85c348b3e2f20507baf
Andrew Hass <ahass@jellyvision.com>
2014-09-23 15:39:29 -0500

    Now just using jQuery's extend() to deep merge.
    Duh.

--- packages/web-services/public/controllers/requests.js
+++ packages/web-services/public/controllers/requests.js
@@ -95,7 +95,7 @@ angular.module('mean.web-services').controller('RequestsController', ['$scope', 
 
 
                 $scope.expecteddata = $scope.actualdata;
-                $scope.expecteddata.body = extend(angular.copy($scope.actualdata.body), xxx);
+                $scope.expecteddata.body = $.extend(angular.copy($scope.actualdata.body), xxx);
 
                 $scope.hasResponse = true;
             });
@@ -124,98 +124,6 @@ angular.module('mean.web-services').controller('RequestsController', ['$scope', 
             });
         };
 
-        var extend = function() {
-            var options, name, src, copy, copyIsArray, clone, target = arguments[0] || {},
-            i = 1,
-            length = arguments.length,
-            deep = false,
-            toString = Object.prototype.toString,
-            hasOwn = Object.prototype.hasOwnProperty,
-            push = Array.prototype.push,
-            slice = Array.prototype.slice,
-            trim = String.prototype.trim,
-            indexOf = Array.prototype.indexOf,
-            class2type = {
-              "[object Boolean]": "boolean",
-              "[object Number]": "number",
-              "[object String]": "string",
-              "[object Function]": "function",
-              "[object Array]": "array",
-              "[object Date]": "date",
-              "[object RegExp]": "regexp",
-              "[object Object]": "object"
-            },
-            jQuery = {
-              isFunction: function (obj) {
-                return jQuery.type(obj) === "function"
-              },
-              isArray: Array.isArray ||
-              function (obj) {
-                return jQuery.type(obj) === "array"
-              },
-              isWindow: function (obj) {
-                return obj != null && obj == obj.window
-              },
-              isNumeric: function (obj) {
-                return !isNaN(parseFloat(obj)) && isFinite(obj)
-              },
-              type: function (obj) {
-                return obj == null ? String(obj) : class2type[toString.call(obj)] || "object"
-              },
-              isPlainObject: function (obj) {
-                if (!obj || jQuery.type(obj) !== "object" || obj.nodeType) {
-                  return false
-                }
-                try {
-                  if (obj.constructor && !hasOwn.call(obj, "constructor") && !hasOwn.call(obj.constructor.prototype, "isPrototypeOf")) {
-                    return false
-                  }
-                } catch (e) {
-                  return false
-                }
-                var key;
-                for (key in obj) {}
-                return key === undefined || hasOwn.call(obj, key)
-              }
-            };
-          if (typeof target === "boolean") {
-            deep = target;
-            target = arguments[1] || {};
-            i = 2;
-          }
-          if (typeof target !== "object" && !jQuery.isFunction(target)) {
-            target = {}
-          }
-          if (length === i) {
-            target = this;
-            --i;
-          }
-          for (i; i < length; i++) {
-            if ((options = arguments[i]) != null) {
-              for (name in options) {
-                src = target[name];
-                copy = options[name];
-                if (target === copy) {
-                  continue
-                }
-                if (deep && copy && (jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)))) {
-                  if (copyIsArray) {
-                    copyIsArray = false;
-                    clone = src && jQuery.isArray(src) ? src : []
-                  } else {
-                    clone = src && jQuery.isPlainObject(src) ? src : {};
-                  }
-                  // WARNING: RECURSION
-                  target[name] = extend(deep, clone, copy);
-                } else if (copy !== undefined) {
-                  target[name] = copy;
-                }
-              }
-            }
-          }
-          return target;
-        }
-
         $scope.updateAssertions = function() {
             $scope.assertions = {};
             for (var item in $scope.assertionMap) {
@@ -238,18 +146,17 @@ angular.module('mean.web-services').controller('RequestsController', ['$scope', 
                         s = s.parent;
                     }
                 }
-                $scope.assertions = extend(true, {}, $scope.assertions, assertion);
+                $scope.assertions = $.extend(true, {}, $scope.assertions, assertion);
             }
 
             var request = $scope.request;
             if (!request.updated) {
                 request.updated = [];
             }
-            console.log($scope.assertions);
-            /*request.updated.push(new Date().getTime());
+            request.updated.push(new Date().getTime());
             request.web_service = request.web_service._id;
             request.assertions = $scope.assertions;
-            request.$update();*/
+            request.$update();
         };
     }
 ])
@@ -376,7 +283,7 @@ angular.module('mean.web-services').controller('RequestsController', ['$scope', 
                     scope.isFlagged = false;
                     delete scope.assertionMap[assertionKey];
                 }
-                console.log(scope.assertionMap);
+
                 return assertionKey;
             };
 
@@ -400,7 +307,6 @@ angular.module('mean.web-services').controller('RequestsController', ['$scope', 
             scope.addItem = function(obj) {
                 if (getType(obj) === 'Object') {
                     // check input for key
-                    console.log(scope);
                     if (scope.keyName === undefined || scope.keyName.length === 0) {
                         alert('Please fill in a name');
                     } else if (scope.keyName.indexOf('$') === 0) {
@@ -461,9 +367,6 @@ angular.module('mean.web-services').controller('RequestsController', ['$scope', 
             };
             scope.possibleNumber = function(val) {
                 return isNumber(val) ? parseFloat(val) : val;
-            };
-            scope.traceP = function(val) {
-                console.log(val);
             };
 
             //////
